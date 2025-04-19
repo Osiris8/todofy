4d8754b74eebec63d279f88f4318611f3068bbb9
Osiris8 <miganosiris8@gmaaail.com>
2025-04-19 19:41:17 +0200

    update doc component

--- components/Doc.tsx
+++ components/Doc.tsx
@@ -1,176 +1,133 @@
 export default function Doc() {
   return (
     <div className="container mx-auto px-4 mt-4">
-      {/* Presentation*/}
-      <div className="collapse bg-base-100 border border-base-300 mb-2 mt-2">
+      <div className="collapse bg-base-100 border border-base-300">
         <input type="radio" name="my-accordion-1" defaultChecked />
         <div className="collapse-title font-semibold">Use your API Client</div>
         <div className="collapse-content text-sm">
-          <p>
-            Use Postman or your favorite API Client{" "}
-            <ul className="list-disc ml-8">
-              <li>
-                {" "}
-                <a
-                  href="https://hoppscotch.io/"
-                  className="link link-primary"
-                  target="_blank"
-                >
-                  https://hoppscotch.io/
-                </a>
-              </li>
-              <li>
-                {" "}
-                <a
-                  href="https://insomnia.rest/"
-                  className="link link-primary"
-                  target="_blank"
-                >
-                  https://insomnia.rest/
-                </a>
-              </li>
-            </ul>
-          </p>
+          <p>Use Postman or your favorite API Client</p>
+          <ul className="list-disc ml-8">
+            <li>
+              <a href="https://hoppscotch.io/" className="link link-primary">
+                https://hoppscotch.io/
+              </a>
+            </li>
+            <li>
+              <a href="https://hoppscotch.io/" className="link link-primary">
+                https://insomnia.rest/
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
 
-      {/* Create todo List */}
-      <div className="collapse bg-base-100 border border-base-300 mb-2 mt-2">
-        <input type="radio" name="my-accordion-1" />
+      <div className="collapse bg-base-100 border border-base-300">
+        <input type="radio" name="my-accordion-1" defaultChecked />
         <div className="collapse-title font-semibold">Create Todo List</div>
         <div className="collapse-content text-sm">
-          <p>
-            Send request
-            <ul className="list-disc ml-8">
-              <li>Method : POST</li>
-              <li>
-                Url :{" "}
-                <a
-                  href="https://insomnia.rest/"
-                  className="link link-primary"
-                  target="_blank"
-                >
-                  https://insomnia.rest/
-                </a>
-              </li>
-              <li>
-                Parameter JSON example :{" "}
-                <div className="mockup-code w-full mt-4">
-                  <pre>
-                    <code>
-                      {"{"} "text": "Hello World" {"}"}
-                    </code>
-                  </pre>
-                </div>
-              </li>
-            </ul>
-          </p>
+          <p>Send request</p>
+          <ul className="list-disc ml-8">
+            <li>Method : POST</li>
+            <li>
+              Url :{" "}
+              <a href="https://insomnia.rest/" className="link link-primary">
+                https://insomnia.rest/
+              </a>
+            </li>
+            <li>
+              Parameter JSON example :
+              <div className="mockup-code w-full">
+                <pre data-prefix="$">
+                  <code>
+                    {"{"} "text": "Hello World" {"}"}
+                  </code>
+                </pre>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
 
-      {/* Read todo List */}
-      <div className="collapse bg-base-100 border border-base-300 mb-2 mt-2">
-        <input type="radio" name="my-accordion-1" />
+      <div className="collapse bg-base-100 border border-base-300">
+        <input type="radio" name="my-accordion-1" defaultChecked />
         <div className="collapse-title font-semibold">Read Todo List</div>
         <div className="collapse-content text-sm">
-          <p>
-            Send request
-            <ul className="list-disc ml-8">
-              <li>Method : GET</li>
-              <li>
-                Url :{" "}
-                <a
-                  href="https://insomnia.rest/"
-                  className="link link-primary"
-                  target="_blank"
-                >
-                  https://insomnia.rest/
-                </a>
-              </li>
-            </ul>
-          </p>
+          <p>Send request</p>
+          <ul className="list-disc ml-8">
+            <li>METHOD : GET</li>
+            <li>
+              Url :
+              <a href="https://hoppscotch.io/" className="link link-primary">
+                https://insomnia.rest/
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
 
-      {/* Update todo List */}
-      <div className="collapse bg-base-100 border border-base-300 mb-2 mt-2">
-        <input type="radio" name="my-accordion-1" />
+      <div className="collapse bg-base-100 border border-base-300">
+        <input type="radio" name="my-accordion-1" defaultChecked />
         <div className="collapse-title font-semibold">Update Todo List</div>
         <div className="collapse-content text-sm">
-          <p>
-            After to make GET, Send request using your todo list ID
-            <ul className="list-disc ml-8">
-              <li>Method : PATCH</li>
-              <li>
-                Url :{" "}
-                <a
-                  href="https://insomnia.rest/"
-                  className="link link-primary"
-                  target="_blank"
-                >
-                  https://insomnia.rest/your-todo-list-id
-                </a>
-              </li>
-              <li>
-                Parameter JSON example :{" "}
-                <div className="mockup-code w-full mt-4">
-                  <pre>
-                    <code>
-                      {"{"} "text": "Update my todo list with my todo list ID"{" "}
-                      {"}"}
-                    </code>
-                  </pre>
-                </div>
-              </li>
-            </ul>
-          </p>
+          <p>After to make GET, Send request using your todo list ID</p>
+          <ul className="list-disc ml-8">
+            <li>Method : PATCH</li>
+            <li>
+              Url :{" "}
+              <a href="https://insomnia.rest/" className="link link-primary">
+                https://insomnia.rest/your-todo-list-id
+              </a>
+            </li>
+            <li>
+              Parameter JSON example :
+              <div className="mockup-code w-full">
+                <pre data-prefix="$">
+                  <code>
+                    {"{"} "text": "Update my todo list with my todo list ID"{" "}
+                    {"}"}
+                  </code>
+                </pre>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
 
-      {/* Validate todo : toogle TODO*/}
-      <div className="collapse bg-base-100 border border-base-300 mb-2 mt-2">
-        <input type="radio" name="my-accordion-1" />
-        <div className="collapse-title font-semibold">Toogle Todo List</div>
+      <div className="collapse bg-base-100 border border-base-300">
+        <input type="radio" name="my-accordion-1" defaultChecked />
+        <div className="collapse-title font-semibold">Toggle Todo List</div>
         <div className="collapse-content text-sm">
-          <p>
-            Send request using your todo list ID
-            <ul className="list-disc ml-8">
-              <li>Method : PATCH</li>
-              <li>
-                Url :{" "}
-                <a
-                  href="https://insomnia.rest/"
-                  className="link link-primary"
-                  target="_blank"
-                >
-                  https://insomnia.rest/your-todo-list-id
-                </a>
-              </li>
-            </ul>
-          </p>
+          <p>Send request using your todo list ID</p>
+          <ul className="list-disc ml-8">
+            <li>METHOD : PATCH</li>
+            <li>
+              <a
+                href=" https://insomnia.rest/your-todo-list-id"
+                className="link link-primary"
+              >
+                https://insomnia.rest/your-todo-list-id
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-      {/*Delete todo List*/}
-      <div className="collapse bg-base-100 border border-base-300 mb-2 mt-2">
-        <input type="radio" name="my-accordion-1" />
+
+      <div className="collapse bg-base-100 border border-base-300">
+        <input type="radio" name="my-accordion-1" defaultChecked />
         <div className="collapse-title font-semibold">DELETE Todo List</div>
         <div className="collapse-content text-sm">
-          <p>
-            Send request using your todo list ID
-            <ul className="list-disc ml-8">
-              <li>Method : DELETE</li>
-              <li>
-                Url :{" "}
-                <a
-                  href="https://insomnia.rest/"
-                  className="link link-primary"
-                  target="_blank"
-                >
-                  https://insomnia.rest/your-todo-list-id
-                </a>
-              </li>
-            </ul>
-          </p>
+          <p>Send request using your todo list ID</p>
+          <ul className="list-disc ml-8">
+            <li>METHOD : DELETE</li>
+            <li>
+              <a
+                href=" https://insomnia.rest/your-todo-list-id"
+                className="link link-primary"
+              >
+                https://insomnia.rest/your-todo-list-id
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
